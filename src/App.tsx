e70a2912cf67f6cea358a1ebf49beb941c129a9d
abhay113 <abhay@cateina.com>
2025-08-07 11:33:52 +0530

    yes bank

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="landing-container">
+      <img
+        src='https://upload.wikimedia.org/wikipedia/commons/thumb/4/4f/Yes_Bank_SVG_Logo.svg/300px-Yes_Bank_SVG_Logo.svg.png?20141004043941'
+        alt="YES BANK Logo"
+        className="yesbank-logo"
+      />
+      <h1>Welcome to YES BANK</h1>
+      <p>Your trusted partner in banking and financial services.</p>
+    </div>
+  );
 }
 
-export default App
+export default App;
